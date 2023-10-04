83ea7766dfc983dee4318cffc9f7bce2bdb6159b
Jihed <zeldaspirit1@gmail.com>
2023-10-04 10:15:15 +0100

    git

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.set("TOKEN_SECRET", `${process.env.TOKEN_SECRET}`);
 // console.log(app.get("TOKEN_SECRET"));
 app.use(logger("dev"));
-console.log("jihed");
+console.log("jdidi");
 app.use(bodyparser.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.json({ Welcome: "to node.js" });
