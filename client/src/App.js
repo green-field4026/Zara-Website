1d0c47a1f2e778333f28042503fa7391a181df66
irma-ahmed <ahmedirmani@yahoo.fr>
2023-10-04 16:09:48 +0100

    updating

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage"
 import SignUp from "./components/SignUp"
 import LoginPage from "./components/LoginPage";
-// import Contact from "./components/Contact"
+import Contact from "./components/Contact"
 import AboutUs from "./components/AboutUs";
 const App = () => {
   return (
@@ -13,7 +13,7 @@ const App = () => {
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
-        {/* <Route path="/contact" element={<Contact />}></Route> */}
+        <Route path="/contact" element={<Contact />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
       </Routes>
     </div>
---
+++ client/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from './Footer'
+import Navbar from './Navbar'
+import TopHearder from './TopHearder'
+
+const Contact = () => {
+  return (
+    <div> 
+        <TopHearder/>
+        <Navbar/>
+    
+  <Footer/>
+  </div>
+  )
+}
+
+export default Contact
