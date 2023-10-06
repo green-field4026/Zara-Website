bc35e5840fa665f39c930840180c19d18ca259aa
Jihed <zeldaspirit1@gmail.com>
2023-10-06 17:08:20 +0100

    fix and shit

--- client/src/components/WishlistCards.js
+++ client/src/components/WishlistCards.js
@@ -1,47 +1,46 @@
-import React from 'react'
+import React from "react";
 import {
-    FaShoppingCart,
-    FaRegBookmark,
-    FaStar,
-    FaFireAlt,
-  } from "react-icons/fa";
-  import { Link } from "react-router-dom";
+  FaShoppingCart,
+  FaRegBookmark,
+  FaStar,
+  FaFireAlt,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const WishlistCards = ({ remove, oneElement, index }) => {
   
-
-
-
-const WishlistCards = ({ oneElement, index}) => {
-  
+  const Users = JSON.parse(localStorage.getItem("user"));
+  const uId = Users.id;
   return (
- <div key={index} className="productCard">
-    <Link to="/details" state={{ from: oneElement }} className="cardlink">
+    <div key={index} className="productCard">
+      <Link to="/details" state={{ from: oneElement }} className="cardlink">
         <img
           src={oneElement.Images[3].image_Url}
           alt="product-img"
           className="productImage"
         ></img>
       </Link>
-          <i class="fa-solid fa-trash productCard__cart cardicons"></i>
-        <div className="productCard__content">
-          <h3 className="productName">{oneElement.name}</h3>
-          <div className="displayStack__1">
-            <div className="productPrice">${oneElement.price}</div>
-            <div className="productSales">
-              {oneElement.stockNumber} in Stock
-            </div>
+      <i
+        onClick={() => (remove(oneElement.id, uId))}
+        class="fa-solid fa-trash productCard__cart cardicons"
+      ></i>
+      <div className="productCard__content">
+        <h3 className="productName">{oneElement.name}</h3>
+        <div className="displayStack__1">
+          <div className="productPrice">${oneElement.price}</div>
+          <div className="productSales">{oneElement.stockNumber} in Stock</div>
+        </div>
+        <div className="displayStack__2">
+          <div className="productRating">
+            {[...Array(Math.trunc(oneElement.rate))].map((index, i) => (
+              <FaStar id={`${index + 1}`} key={i} />
+            ))}
           </div>
-          <div className="displayStack__2">
-            <div className="productRating">
-              {[...Array(Math.trunc(oneElement.rate))].map((index, i) => (
-                <FaStar id={`${index + 1}`} key={i} />
-              ))}
-            </div>
-            <div className="productTime">{oneElement.rate}</div>
-          </div>
+          <div className="productTime">{oneElement.rate}</div>
         </div>
       </div>
-    
+    </div>
   );
 };
 export default WishlistCards;
-
