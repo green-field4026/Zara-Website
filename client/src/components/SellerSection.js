b49b4002e49c172d6a29b6d990ae371a9b149dfa
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2023-10-08 12:08:02 +0100

    mychanges

--- client/src/components/SellerSection.js
+++ client/src/components/SellerSection.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Footer from "./Footer";
 import TopHeader from "./TopHearder";
 import HomeNav from "./HomeNav";
-
+import "../styles/Seller.css"
 function SellerSection({ addProdSeller }) {
   const [name, setName] = useState('')
   const [quantity, setQuantity] = useState(1)
@@ -20,12 +20,26 @@ function SellerSection({ addProdSeller }) {
     <div>
       <TopHeader />
       <HomeNav />
+      <div className='container'>
 
-      <label>Your product:</label><br></br>
-      <span>Name:</span><input placeholder='name of the product ' onChange={(e) => { setName(e.target.value);  }} /><br></br>
-      <span>Quantity:</span><input defaultValue={1} type='number' placeholder='number of quantity' onChange={(e) => { setQuantity(e.target.value) }} /><br></br>
-      <span>Price :</span><input defaultValue={1} type="number" placeholder='Price of the product' onChange={(e) => { setPrice(e.target.value) }} /><br></br>
-      <span>Description:</span><input placeholder='description of the product' onChange={(e) => { setDescription(e.target.value) }} /><br></br>
+      <div className="title">
+          <div className="carre"></div>
+          <span className="titre">Featured</span>
+        </div>
+        <div className="before-cards">
+          <div className="timer">
+            <h1>New Arrival</h1>
+          </div>
+        </div>
+
+     <div className='sellersection'>
+      <div className='theinputs'>
+        <div className='inputse'>
+      <label>Your product:</label>
+      <span>Name:</span><input placeholder='name of the product ' onChange={(e) => { setName(e.target.value);  }} />
+      <span>Quantity:</span><input defaultValue={1} type='number' placeholder='number of quantity' onChange={(e) => { setQuantity(e.target.value) }} />
+      <span>Price :</span><input defaultValue={1} type="number" placeholder='Price of the product' onChange={(e) => { setPrice(e.target.value) }} />
+      <span>Description:</span><input placeholder='description of the product' onChange={(e) => { setDescription(e.target.value) }} />
       <span>Category:</span><select onChange={(e) => { setCategory(e.target.value);  }}>
         <option value=" ">Select...</option>
         <option value="Woman's Fashion">Woman's Fashion</option>
@@ -43,26 +57,29 @@ function SellerSection({ addProdSeller }) {
         <option value="Headphones">Headphones</option>
         <option value="Gaming">Gaming</option>
 
-      </select>    <br></br>
+      </select>    
       <span>Avaiblility:</span><select onChange={(e) => { setAvailibility(e.target.value) }}>
         <option value="in Stock">in Stock</option>
         <option value="out Of Stock">out Of Stock</option>
-      </select><br></br>
+      </select>
+</div>
 
-
-
+     <div>
       <label>add 4 images of your product :</label><br></br>
       <span></span><input onChange={(e) => { setFImage(e.target.value);  }} placeholder='First image' /><br></br>
       <span></span><input onChange={(e) => { setSImage(e.target.value); }} placeholder='Seecond image' /><br></br>
       <span></span><input onChange={(e) => { setTImage(e.target.value); }} placeholder='Third image' /><br></br>
       <span></span><input onChange={(e) => { setFoImage(e.target.value)}} placeholder='Forth image' />
-      <button type='submit' onClick={() => {
+      </div>
+</div>
+</div>
+<button type='submit' onClick={() => {
 addProdSeller({ name:name, stockNumber: quantity*1, price:price*1,rate:rate*1, desc: description, category:category, state: avaiblility }
           , [fImage, sImage, tImage, foImage])
       }}>Add Product</button>
-
-      <Footer />
-    </div>
+</div>
+      <Footer /></div>
+    
   )
 }
 
